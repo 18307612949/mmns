4f816c439aa34ff522b6e02a62a5fe41ba0048f7
Brian Chrzanowski <chrzanowski.kernel.dev@gmail.com>
2017-04-20 19:32:43 -0400

    added the asymptotic curve for computing the time delay between updates

--- funclib.c
+++ funclib.c
@@ -1,13 +1,29 @@
-/*
- * most of the functions in the library exist because I want nice string
- * checking functions. C doesn't come prepackaged with them, so I'll write my
- * own.
- */
 
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
 
+/* 
+ * these functions are integral to the operation of the program, as they
+ * help determine the delay in which nodes are allowed to request information,
+ */
+
+double compute_time_delay(int users, int nodes)
+{
+	// we have to check function's inputs. this is mostly when there are 0
+	// active people online
+	if (users < 1) {
+		users = 1;
+	}
+
+	if (nodes < 1) {
+		nodes = 1;
+	}
+
+    return (((double)nodes / users)) * 60;
+}
+
+/* the functions here exist because I wanted them to exist. As simple as that */
 int isnumeric(char *input)
 {
 	int i;
