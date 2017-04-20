119ff0540cb8298a4c1bf407696e981682ce4fa5
Brian Chrzanowski <chrzanowski.kernel.dev@gmail.com>
2017-04-20 19:38:20 -0400

    moved blocked comment to funclib.c

--- funclib.c
+++ funclib.c
@@ -1,11 +1,49 @@
-
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
 
-/* 
- * these functions are integral to the operation of the program, as they
- * help determine the delay in which nodes are allowed to request information,
+/*
+ * ---------- A-NOTE-TO-PEOPLE-FOLLOWING-THE-LOGIC-OF-THIS-PROGRAM -----------
+ *
+ * Note that the needs of interactive load-balancing require more information
+ * acquired from individual nodes as more users connect to the system. This
+ * is to ensure that people are assigned a capable machine for WHATEVER task is
+ * required of them.
+ *
+ *		more users -> more data / minute
+ *
+ * The curve of data flow can be described by the following function:
+ *
+ *		(((double)Number Of Nodes/Number Of Users)) * 60
+ *
+ *		Where
+ *			1 <= Number Of Nodes <= inf
+ *			1 <= Number Of Users <= inf
+ *
+ * This interior function is of the form 1/X, which has an asymptotic nature.
+ * Altering the constant with the number of available nodes simply changes the
+ * stretch in the function. When multiplying by 60, this obtains a number of
+ * seconds each mmns client needs to sleep between blasting updates.
+ *
+ * Examples using a "small" 6 node system:
+ *
+ *		Work Day:
+ *			60 Employees using 6 nodes
+ *				(6 / 60) * 60 = Update every 6 seconds
+ *
+ *		Night Shift:
+ *			4 Employees using 6 nodes
+ *				(6 / 4) * 60  = Update every 90 seconds
+ *
+ *		Saturday:
+ *			18 Employees using 6 nodes
+ *				(6 / 18) * 60 = Update every 20 seconds
+ *
+ * Examples using a "massive" 300 nodes system:
+ *
+ *		Work Day:
+ *			8000 Employees using 300 nodes
+ *				(300 / 8000) * 60 = Update every 2.25 seconds
  */
 
 double compute_time_delay(int users, int nodes)
